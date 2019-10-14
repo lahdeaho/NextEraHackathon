332ba9897ebcbb4fe0ad02199c2c865915296285
Mikko Lähdeaho <mikko.lahdeaho@invenco.fi>
2019-10-14 22:20:19 +0000

    web app improved

--- webapp/project/static/js/index.js
+++ webapp/project/static/js/index.js
@@ -1,17 +1,62 @@
 var cammap;
 
 $(function() {
-    cammap = L.map('heat-map', { zoomControl: false }).setView([65.1733244, 24.9410248], 4);
-    L.control.zoom({ position: 'bottomleft' }).addTo(cammap);
+    cammap = L.map('heat-map', { zoomControl: false }).setView([65.0, 25.5], 10);
+    L.control.zoom({ position: 'topleft' }).addTo(cammap);
+    var h = 0.0033
+    var v = 0.0014
+    var ndvi_curr_url = '/static/map_images/heatmap_rgba.png',
+        ndvi_month_url = '/static/map_images/heatmap.png',
+        ndvi_half_url = '/static/map_images/stadium_rgba.png',
+        ndvi_year_url = '/static/map_images/heatmap_rgba.png',
+        imageBounds = [
+                        L.latLng(65.8214035188828 + v, 24.869962219258937 + h),
+                        L.latLng(64.8214388312971 + v, 27.213549565024742 + h)
+    ];
+    
+    /*
+                            L.latLng(65.8214035188828, 24.869962219258937),
+                        L.latLng(64.8214388312971, 27.213549565024742)
+    */
+/*
+    var mapLink = 
+            '<a href="http://www.esri.com/">Esri</a>';
+    var wholink = 
+        'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+    L.tileLayer(
+        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: '&copy; '+mapLink+', '+wholink,
+        maxZoom: 18,
+        }).addTo(cammap);
+*/
     L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=" +
-                "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
-                    maxZoom: 18,
-                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    id: 'mapbox.streets'
+            "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox.streets'
     }).addTo(cammap);
     
+    var ndvi_curr = L.imageOverlay(ndvi_curr_url, imageBounds, { opacity: 0.9 });
+    var ndvi_month = L.imageOverlay(ndvi_month_url, imageBounds, { opacity: 0.9 });
+    var ndvi_half = L.imageOverlay(ndvi_half_url, imageBounds, { opacity: 0.9 });
+    var ndvi_year = L.imageOverlay(ndvi_year_url, imageBounds, { opacity: 0.9 });
+    
+    var overlayMaps = {
+        "NDVI current": ndvi_curr,
+        "NDVI month to date": ndvi_month,
+        "NDVI half year to date": ndvi_half,
+        "NDVI year to date": ndvi_year
+    };
+
+    /*
+    var pinMaps = {
+        "Alerts": alerts
+    };*/
+    
+    L.control.layers(overlayMaps).addTo(cammap);
+/*    
     var markers = L.markerClusterGroup();
     var markersCoords = {};
     var geojsonMarkerOptions = {
@@ -62,4 +107,5 @@ $(function() {
             console.log(error);
         }
     });
+*/
 });
